bbf5f0b6984e30fe2cbd74371a18a9d10be9e0b2
RatSpaceSteam <145033829+RatSpaceSteam@users.noreply.github.com>
2024-09-17 08:29:06 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main{
     public static void main(String[]args){
-        
+        Car auto = new Car(15);
+        System.out.println("New car odometer reading: " + auto.getOdometerReading());
+        auto.fillUp(150,8);
+        System.out.println("Miles per gallon: " + auto.calculateMPG());
+        System.out.println("Miles per gallon: " + auto.calculateMPG());
+        auto.resetMPG();
+        auto.fillUp(350, 10);
+        auto.fillUp(450, 20);
+        System.out.println("Miles per gallon: " + auto.calculateMPG());
+        auto.resetMPG();
+        auto.fillUp(603, 25.5);
+        System.out.println("Miles per gallon: " + auto.calculateMPG());
     }
 }
